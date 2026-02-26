4b93a14b8199d4811da75476fc5e40f8bc7af33f
metumu <metumu@madamholdings.com>
2026-02-26 00:58:17 +0000

    Deploy changes

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,54 +1,30 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+  # Which packages should be installed in your workspace?
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.nodejs_20
+    pkgs.firebase-tools
+    pkgs.git
   ];
-  # Sets environment variables in the workspace
-  env = {};
-  idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      # "vscodevim.vim"
-      "google.gemini-cli-vscode-ide-companion"
-    ];
-    # Enable previews
+
+  # Look for extensions at https://open-vsx.org/
+  idx.extensions = [
+    "esbenp.prettier-vscode" # Keeps your elite code clean and professional
+    "google.idx-ai-extension" # Enables Gemini assistance directly in the IDE
+    "ritwickdey.LiveServer" # To preview your work before pushing to Firebase
+  ];
+
+  # Enable previews so you can see the site as you build
+  idx.previews = {
+    enable = true;
     previews = {
-      enable = true;
-      previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
-    };
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-        # Open editors for the following files by default, if they exist:
-        default.openFiles = [ ".idx/dev.nix" "README.md" ];
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
+      web = {
+        # This command runs a simple server to show your HTML/JS files
+        command = ["npx" "http-server" "." "-p" "$PORT"];
+        manager = "web";
       };
     };
   };
 }
+
